aba1c3de5e191c19a2a8da94bed1b9370012baff
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-13 10:55:23 -0700

    Switched base class to one from FCC-PHP-Libraries, tiny bit of code cleanup

--- APIBaseClass.php
+++ APIBaseClass.php
@@ -1,61 +1,58 @@
-<?php 
+<?php
 class APIBaseClass {
- 
-	// API base URL.
-	protected $base_url;
-
-	// cURL handle used to make API request.
-	protected $ch;
-
-	// API output and info.
-	protected $output;
-	protected $info;
-
-	/**
-	 * Class constructor.
-	 * @param string $base_url
-	 * @return null
-	 */
-	protected function __construct($base_url) {
-		$this->base_url = $base_url;
-		$this->ch = curl_init();
+	private $_root, $_http;
+	
+	public function __construct($server, $http = false) {
+		$this->_root = $server;
+		if($http) {
+			$this->_http = $http;
+		} else {
+			$this->_http = curl_init();
+			curl_setopt($this->_http, CURLOPT_RETURNTRANSFER, 1);
+		}
 	}
+	
+	private function _request($path, $method, $data=false, $headers=false) {
+		# URL encode any available data
+        if ($data) $query = http_build_query($data);
+		
+		// these statements could be combined.. but not ready to take the leap
+		if(in_array(strtolower($method), array('get','delete'))) 
+			# Add urlencoded data to the path as a query if method is GET or DELETE
+			if($data) $path = $path.'?'.$query;
+		else {
+			# If method is POST or PUT, put the query data into the body
+			$body = ($data) ? $query : '';
+			curl_setopt($this->_http, CURLOPT_POSTFIELDS, $body);
+		}
+		
+		$url = $this->_root . $path;
+		curl_setopt($this->_http, CURLOPT_URL, $url);
+		if($headers) curl_setopt($this->_http, CURLOPT_HTTPHEADER, $headers);
 
-	/**
-	 * Get the output of a cURL request.
-	 * @return string
-	 * 
-	 */
-	public function getOutput() {
-		return $this->output;
+		curl_setopt($this->_http, CURLOPT_CUSTOMREQUEST, $method);
+
+		$result = curl_exec($this->_http);
+	
+		if($result === false) {	
+			echo 'Curl error: ' . curl_error($this->_http) . "\n";
+		} 
+		//curl_close($this->_http);
+		
+		return $result;
+		
 	}
-
-	/**
-	 * Get information about the last cURL request made.
-	 * @return string
-	 */
-	public function getInfo() {
-		return $this->info;
+	
+	public function get($path, $data, $headers = null) {
+		return $this->_request($path, 'GET', $data, $headers);
 	}
-
-	/**
-	 * Method to log API request outcome.
-	 * Can be overriden in derived classes for custom logging.
-	 * @param string $logFile
-	 * @param string $message
-	 * @return null
-	 */
-	protected function logResults($logFile, $message) {
-		$loghandle = fopen($logFile, 'a');
-		fwrite($loghandle, date("F j, Y, g:i a").": API response from ".$this->base_url." = ".$message."\n");
-		fclose($loghandle);
+	public function post($path, $data, $headers = null) {
+		return $this->_request($path, 'POST', $data, $headers);
 	}
-
-	/**
-	 * Class destructor.
-	 *
-	 */
-	protected function __destruct() {
-		curl_close($this->ch);
+	public function put($path, $data, $headers = null) {
+		return $this->_request($path, 'PUT', $data, $headers);
+	}
+	public function delete($path, $data, $headers = null) {
+		return $this->_request($path, 'DELETE', $data, $headers);
 	}
 }
