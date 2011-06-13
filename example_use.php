e4366f192b785a242b24faebc3658d0d67d9e8e0
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-13 13:44:44 -0700

    Made example_use.php more useful by adding some created object information.

--- example_use.php
+++ example_use.php
@@ -1,10 +1,11 @@
 <?php
-echo 'hello world';
+// simple wrapper for api classes. dies with object information (object values,methods and variables)
+
+// Base API Class
 require 'APIBaseClass.php';
+// Custom API Class
 require 'example_api.php';
 
-$new =new FCCApi();
-echo 'hello';
-
-die(print_r($new));
-
+$new = new FCCApi();
+// Debug information
+die(print_r($new).print_r(get_object_vars($new)).print_r(get_class_methods(get_class($new))));
