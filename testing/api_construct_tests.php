f7fc5e8d2ced921fd8e8b20e309e40066dd648e5
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-15 13:29:30 -0700

    fixed minor syntax errors... compiles properly

--- testing/api_construct_tests.php
+++ testing/api_construct_tests.php
@@ -8,31 +8,30 @@ require_once('../api_class_template.php');
 class TestOfApiConnect extends UnitTestCase {
    
     function testApiConstructs(){
-    	$api = new yourApi();
+    	$this->api = new yourApi();
     	// obviously will return false because these are private parameters
     	self::check_class_params('_http _root',false);
     	self::check_class_params('url',true);
     }
     
     function check_class_params($params=NULL,$mode=TRUE){
-    	if($mode == FALSE)
+    	
     	// look up parameters inside of class and see if they are set/ true
     	// also allow to only check for certain parameters by passing in an array with the names of those variables or a space seperated string
     	// parameters to look for in the object
-    	if($params != null){
-    		if(is_string($params))
-    			$params = explode(' ',$params);
-    		foreach($params as $key_name)
-    			$api_vars [$key_name] = '';
-    		}
+    	if($params != null && is_string($params)){    		
+			$params = explode(' ',$params);
+			foreach($params as $key_name)
+				$api_vars [$key_name] = '$key_name';
+    	}
     	else
     		$api_vars = get_class_vars(get_class($this->api));
     		
     	foreach($api_vars as $key=>$value){
     		if($mode == TRUE)
-    			$this->assertTrue($this->api->$key);
-    		else($mode == FALSE)
-    			$this->assertFalse($this->api->$key);
+    			$this->assertTrue($this->api->$value);
+    		elseif($mode == FALSE)
+    			$this->assertFalse($this->api->$value);
     		}
     }
 }
