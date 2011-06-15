848225a2a72ed879acffd7d594252d4b63256415
Alan Palazzolo <github@zzolo.org>
2011-06-15 16:07:10 -0700

    Added a bit more docs in test index.

--- testing/index.php
+++ testing/index.php
@@ -2,6 +2,12 @@
 /**
  * @file
  * Creates a single place to run all tests
+ *
+ * This testing utilizes SimpleTest.  This should be included in this
+ * library.
+ *
+ * See the following page for API documentation on SimpleTest:
+ * http://www.simpletest.org/api/
  */
 
 require_once './unit_APIBaseClass.php';
