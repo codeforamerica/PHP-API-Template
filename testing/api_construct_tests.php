62e3431c87da632173ea05b3e3ea702da2fd1dbd
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2011-06-15 12:19:31 -0700

    made some basic changes in syntax, removed the construct with url because its not possible to automatically pass a parameter in tests, unless the class handles it, added mode to check class parameters

--- testing/api_construct_tests.php
+++ testing/api_construct_tests.php
@@ -6,17 +6,16 @@ require_once('../APIBaseClass.php');
 require_once('../api_class_template.php');
 // the name of the api class is 'yourApi'
 class TestOfApiConnect extends UnitTestCase {
-    function testApiConstructs() {
-        // create the object and use the assertFalse assertTrue to check to see what happened
-        $this->api = new yourApi();
-        self::check_class_params('_http _root url');
-    }
-    function testApiConstructsWithPassedUrl($url){
-    	$api = new yourApi($url);
-    	self::check_class_params('_http _root url');
+   
+    function testApiConstructs(){
+    	$api = new yourApi();
+    	// obviously will return false because these are private parameters
+    	self::check_class_params('_http _root',false);
+    	self::check_class_params('url',true);
     }
     
-    function check_class_params($params=NULL){
+    function check_class_params($params=NULL,$mode=TRUE){
+    	if($mode == FALSE)
     	// look up parameters inside of class and see if they are set/ true
     	// also allow to only check for certain parameters by passing in an array with the names of those variables or a space seperated string
     	// parameters to look for in the object
@@ -29,8 +28,12 @@ class TestOfApiConnect extends UnitTestCase {
     	else
     		$api_vars = get_class_vars(get_class($this->api));
     		
-    	foreach($api_vars as $key=>$value)
-    		$this->assertTrue($this->api->$key);
+    	foreach($api_vars as $key=>$value){
+    		if($mode == TRUE)
+    			$this->assertTrue($this->api->$key);
+    		else($mode == FALSE)
+    			$this->assertFalse($this->api->$key);
+    		}
     }
 }
 ?>
